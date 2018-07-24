f010cfa1766efeecab3e5eeb966bb16c553764a0
Eli Sandine <eli.sandine@gmail.com>
2018-07-24 16:18:48 -0700

    View is now saving form data to the db.

--- food/views.py
+++ food/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
 
 from .forms import AddItem
 from .models import FoodItem
@@ -14,22 +14,32 @@ def index(request):
     # Get a list of food based on specific user that is logged in
     food_list = FoodItem.objects.filter(user_id=request.user).order_by('-date_added')
     
+    add_item_form = AddItem()
 
     if request.method == 'POST':
         add_item_form = AddItem(request.POST)
-        print("STUFF: " + str(request.POST))
+
         if add_item_form.is_valid():
-            view = 'food/added.html'
-            print("AYYYYOOO")
+            item =  add_item_form.save(commit=False)
+            item.user = request.user
+            item.save()
+
+           # view = 'food/added.html'
+           # food_item = add_item_form.cleaned_data['item']
+           # date = add_item_form.cleaned_data['date_added']
+           # user_inputs = [food_item, date]
+
+            context = {
+                'latest_food_items': food_list,
+                'add_form': add_item_form, 
+            }
 
     else:
-        add_item_form = AddItem()
     
-    
-    context = {
+        context = {
             'latest_food_items': food_list,
             'add_form': add_item_form, 
-    }            
+        }            
 
 
     return render(request, view, context)
