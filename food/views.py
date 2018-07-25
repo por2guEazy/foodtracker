9059c4849d37b1dbfe1f21f86104dd2b594a3b4e
Eli Sandine <eli.sandine@gmail.com>
2018-07-25 15:33:47 -0700

    Added button to go to homepage. Cleaned up some html.

--- food/views.py
+++ food/views.py
@@ -5,30 +5,24 @@ from django.shortcuts import render
 from .forms import AddItem, EditProfile
 from .models import FoodItem, UserProfile
 
+
 # Index view - required login 
 @login_required(login_url='/food/login/')
 def index(request):
-
     view = 'food/index.html'
     # Get a list of food based on specific user that is logged in
     food_list = FoodItem.objects.filter(user_id=request.user).order_by('-date_added')
-    #print(food_list)
-    
+    # Create form
     add_item_form = AddItem()
 
     if request.method == 'POST':
         add_item_form = AddItem(request.POST)
-
+        # Validate data
         if add_item_form.is_valid():
             item =  add_item_form.save(commit=False)
             item.user = request.user
             item.save()
-
-           # view = 'food/added.html'
-           # food_item = add_item_form.cleaned_data['item']
-           # date = add_item_form.cleaned_data['date_added']
-           # user_inputs = [food_item, date]
-
+            
             context = {
                 'latest_food_items': food_list,
                 'add_form': add_item_form, 
@@ -46,24 +40,22 @@ def index(request):
 @login_required(login_url='/food/login/')
 def profile(request):
     view = 'food/profile.html'
+    # Make query to get user info
     user_info = UserProfile.objects.filter(user_id=request.user).first()
-    form_data = EditProfile()
-    # Round BMI to two decimal places
+    # Create form 
+    form_data = EditProfile(instance=user_info)
+    # Calculate & round BMI to 2 decimals 
     bmi = str(round( ((user_info.weight) / (user_info.height**2)) * 703, 2))
 
     if request.method == 'POST':
+        # Check if that instance exists, update or save new data
         form_data = EditProfile(request.POST, instance=user_info)
+        # Validate data
         if form_data.is_valid():
-            # Clean user data
             item =  form_data.save(commit=False)
             item.user = request.user
             item.save()
-            #height = form_data.cleaned_data['height']
-            #if height == 0:
-            #    height = 1
-            #weight = form_data.cleaned_data['weight']
-
-
+           
             context = { 
                 'bmi': bmi,
                 'user_data_form': form_data, 
