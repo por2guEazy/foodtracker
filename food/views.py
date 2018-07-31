73a979189ec9ece27dd05f0fbdf0e35f1a92a8e3
Eli Sandine <eli.sandine@gmail.com>
2018-07-31 13:21:17 -0700

    Cleaned up profile view

--- food/views.py
+++ food/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import string
 import datetime
 from datetime import timedelta
 from django.utils import timezone
@@ -9,11 +9,8 @@ from .forms import AddItem, EditProfile, ViewItemsByDate
 from .models import FoodItem, UserProfile
 
 
-# Index view - required login 
-@login_required(login_url='/food/login/')
-def index(request):
-        
-    # Handle add items by date form
+# Handle the post request and save to db
+def add_post(request):
     if request.method == 'POST':
         add_item_form = AddItem(request.POST)
         # Validate data
@@ -24,29 +21,46 @@ def index(request):
             
             return redirect('index') 
 
+
+
+# Index view - required login 
+@login_required(login_url='/food/login/')
+def index(request):
+        
+    # Handle add items by date form
+    add_post(request)
+
     # Get a list of food based on specific user that is logged in
     # Display items eaten today 
     food_list = FoodItem.objects.filter(
             user_id=request.user, 
-            date_added__gte=datetime.datetime.today().replace(hour=0, minute=0).astimezone()
-    )    
+            date_added__gte=datetime.datetime.today().replace(hour=0, minute=0).astimezone()    
+    ).order_by('-date_added')
+
+
     # Create forms for page
     add_item_form = AddItem()
     view_items_by_date = ViewItemsByDate()
-
     # Get input
     show_by = request.GET.get('field')
+
     # Handle view items by date form
-    if show_by == 'yesterday':
-        food_list = ['Cheese', 'Fake cheese', 'Item']             
+   # if show_by == 'yesterday':
+   #     food_list = FoodItem.objects.filter(
+   #                 user_id=request.user, 
+   #                 date_added__gte=(datetime.datetime.today()-timedelta(days=1) ).replace(hour=0, minute=0).astimezone()    
+   #         ).order_by('-date_added')
+
+
+
+    # Pass data back
     context = {
         'latest_food_items': food_list,
         'add_form': add_item_form,
         'view_by_date_form': view_items_by_date, 
     }
+
     return render(request, 'food/index.html', context)
-
-
 
 
 
@@ -61,17 +75,7 @@ def get_bmi(height, weight):
 
 
 
-# Profile view w/ required login
-@login_required(login_url='/food/login/')
-def profile(request):
-    view = 'food/profile.html'
-    # Make query to get user info
-    user_info = UserProfile.objects.filter(user_id=request.user).first()
-    # Create form 
-    form_data = EditProfile()
-    # Calculate & round BMI to 2 decimals 
-
-
+def profile_post(request, user_info):
     if request.method == 'POST':
         # Check if that instance exists, update or save new data
         form_data = EditProfile(request.POST, instance=user_info)
@@ -80,20 +84,25 @@ def profile(request):
             item =  form_data.save(commit=False)
             item.user = request.user
             item.save()
-             
-            context = { 
-                'bmi': get_bmi(user_info.height, user_info.weight),
-                'user_data_form': form_data, 
-                'user_info': user_info,
-            }
-            
-            return render(request, view, context)
+           
+            return redirect('profile')
 
-  
+
+
+# Profile view w/ required login
+@login_required(login_url='/food/login/')
+def profile(request):
+    # Make query to get user info
+    user_info = UserProfile.objects.filter(user_id=request.user).first()
+    # Handle post request
+    profile_post(request, user_info)
+    # Create form 
+    form_data = EditProfile()
+    # Calculate BMI, and pass data back 
     context = { 
             'bmi': get_bmi(user_info.height, user_info.weight),
             'user_data_form': form_data,
             'user_info': user_info,
     } 
 
-    return render(request, view, context)
+    return render(request, 'food/profile.html', context)
