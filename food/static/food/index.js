6d5f441e852f0bfd931d74eaa9dcb50569ce4295
Eli Sandine <eli.sandine@gmail.com>
2018-08-09 17:46:26 -0700

    Fixing button issues

--- food/static/food/index.js
+++ food/static/food/index.js
@@ -5,27 +5,28 @@ $('.datepicker').datetimepicker({
 
 // Show edit profile field
 $('.show-edit-profile').on('click', function(){
-    $('.edit-profile').removeClass('d-none')
+    $('.edit-profile').removeClass('d-none');
 })
 
 // Hide edit profile fields
 $('.edit-profile-submit').on('click', function() {
-    $('.edit-profile').addClass('d-none')
+    $('.edit-profile').addClass('d-none');
 });
 
 
 // Add current datetime to input field
 $('.add-time-now').on('click', function() {
-    $('#id_date_added').val(moment().format('Y-MM-DD HH:mm'))
+    $('#id_date_added').val(moment().format('Y-MM-DD HH:mm'));
 });
 
 // Display remove btn and done button 
 $('.edit-list').on('click', function() {
-    $('.rmv-item').removeClass('d-none')
-    $('.edit-list-done').removeClass('d-none')
+    $('.rmv-item').removeClass('d-none');
+    $('.edit-list-done').removeClass('d-none');
 });
 
 // Hide rmv buttons 
 $('.edit-list-done').on('click', function() {
-    $('.rmv-item').addClass('d-none')
+    $('.rmv-item').addClass('d-none');
 });
+   
